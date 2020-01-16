09a7edef346275c179b81a6dd8316adb62753d99
AkshayGupt <akshay.189301019@muj.manipal.edu>
2020-01-16 12:51:01 +0530

    Updated Lab2

---
+++ id.sh
@@ -0,0 +1,12 @@
+echo Enter name
+read name
+echo Enter Reg.no
+read reg
+echo Enter Roll
+read roll
+echo Enter class
+read class
+echo Enter year
+read year
+echo "\n"	ID CARD"\n"***********"\n"Name:$name"\n"Reg:$reg"\n"Roll:$roll"\n"Class:$class"\n"Year:$year
+
---
+++ prog2.sh
@@ -0,0 +1,3 @@
+echo Number of files in home directory
+cd $HOME
+ls|wc -l
